1adbe9e5b73c7870e1649d1597d44e783f318571
David Roach <dr@ammirati.com>
2013-10-02 17:55:09 -0400

    Adding Boilerplate
    Signed-off-by: David Roach <dr@ammirati.com>

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/* ------------------------------------------------------------------------
+Ammirati Global Function Setlist
+------------------------------------------------------------------------- */
+var mainJs = (function($){
+
+	// Avoid `console` errors in browsers that lack a console.
+	(function() {
+	    var method;
+	    var noop = function () {};
+	    var methods = [
+	        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+	        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+	        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+	        'timeStamp', 'trace', 'warn'
+	    ];
+	    var length = methods.length;
+	    var console = (window.console = window.console || {});
+	
+	    while (length--) {
+	        method = methods[length];
+	
+	        // Only stub undefined methods.
+	        if (!console[method]) {
+	            console[method] = noop;
+	        }
+	    }
+
+	}());
+	
+	
+
+	
+
+	// PUBLIC METHODS
+	return {
+			
+		init: function(options) {
+			
+			var defaults = {}
+			options = $.extend(defaults, options);	
+
+		}		
+	}
+     
+})(jQuery);
