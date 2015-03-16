fd57d0a1a002dfe4a2ce5de85b8002130ec151ff
David Roach <david.roach@rokkan.com>
2015-03-16 15:41:38 -0400

    Adding/Updating files js
    Signed-off-by: David Roach <david.roach@rokkan.com>

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
-/* ------------------------------------------------------------------------
-Ammirati Global Function Setlist
-------------------------------------------------------------------------- */
-var mainJs = (function($){
-
-	// Avoid `console` errors in browsers that lack a console.
-	(function() {
-	    var method;
-	    var noop = function () {};
-	    var methods = [
-	        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-	        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-	        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-	        'timeStamp', 'trace', 'warn'
-	    ];
-	    var length = methods.length;
-	    var console = (window.console = window.console || {});
-	
-	    while (length--) {
-	        method = methods[length];
-	
-	        // Only stub undefined methods.
-	        if (!console[method]) {
-	            console[method] = noop;
-	        }
-	    }
-
-	}());
-	
-	
-
-	
-
-	// PUBLIC METHODS
-	return {
-			
-		init: function(options) {
-			
-			var defaults = {}
-			options = $.extend(defaults, options);	
-
-		}		
-	}
-     
-})(jQuery);
+var mainJs = (function () {
+ 		
+ 		// Avoid `console` errors in browsers that lack a console.
+		(function() {
+		    var method;
+		    var noop = function () {};
+		    var methods = [
+		        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		        'timeStamp', 'trace', 'warn'
+		    ];
+		    var length = methods.length;
+		    var console = (window.console = window.console || {});
+		
+		    while (length--) {
+		        method = methods[length];
+		
+		        // Only stub undefined methods.
+		        if (!console[method]) {
+		            console[method] = noop;
+		        }
+		    }
+		}());
+ 		
+ 		
+        function init() {
+            //Init Here
+            console.log('init pass');
+        }
+ 
+ 
+        // Reveal public pointers to
+        // private functions and properties
+ 
+        return {
+            init 	:	init
+        };
+})();
