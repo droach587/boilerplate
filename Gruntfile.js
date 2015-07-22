619b90c3b2c309dbdb24dedef4f7d4372a6dee16
David Roach <david.roach@rokkan.com>
2015-07-22 15:37:09 -0400

    Adding updates
    Signed-off-by: David Roach <david.roach@rokkan.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,11 @@
 module.exports = function(grunt) {
-
     // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-        concat: {   
+        concat: {
             dist: {
-                src: [
-                    'js/*.js', // All JS in the folder
-                    'js/global.js'  // This specific file
+                src: ['js/*.js', // All JS in the folder
+                'js/global.js' // This specific file
                 ],
                 dest: 'js/build/app.js',
             }
@@ -18,42 +15,60 @@ module.exports = function(grunt) {
                 src: 'js/build/app.js',
                 dest: 'js/build/app.min.js',
                 options: {
-                   banner: '/* HTML5 JS Boilerplate Five Design Build Date: ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+                    banner: '/* HTML5 JS Boilerplate Five Design Build Date: ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
                 }
             }
         },
-        compass: {                  // Task
-            dist: {                   // Target
-              options: {              // Target options
-                sassDir: 'sass',
-                cssDir: 'css',
-                outputStyle: 'compressed',
-                imagesPath: "img",
-                generatedImagesDir: 'img',
-                fontsPath: "css/fonts",
-                require: 'breakpoint'
+        compass: { // Task
+            dist: { // Target
+                options: { // Target options
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    outputStyle: 'compressed',
+                    imagesPath: "img",
+                    generatedImagesDir: 'img',
+                    fontsPath: "css/fonts",
+                    require: 'breakpoint'
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img/source',
+                    src: ['**/*.{png,jpg,gif,svg}'],
+                    dest: 'img/',
+                    optimizationLevel: 5
+                }],
+                png: {
+                    options: {
+                        optimizationLevel: 7
+                    }
                 }
             }
         },
         watch: {
-          scripts: {
-            files: ['js/*.js'],
-            tasks: ['concat', 'uglify']
-          },
-          styles: {
-              files: ['sass/styles.scss'],
-              tasks: ['compass']
-          }
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify']
+            },
+            images: {
+                files: ['img/source/**/*.{png,jpg,gif}'],
+                tasks: ['imagemin']
+            },
+            styles: {
+                files: ['sass/styles.scss'],
+                tasks: ['compass']
+            }
         }
     });
-
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
-
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch']);
 };
