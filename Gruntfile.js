3cd29999aa29048342b61c693fc392ab7cdab335
David Roach <Droach587@gmail.com>
2015-08-05 21:48:47 -0400

    Updating Build
    Signed-off-by: David Roach <Droach587@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,147 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        /**
-         * Concat JS Files
-         * Contat all files into one
-         * 
-         */
-        concat: {
-            dist: {
-                src: ['js/*.js'],
-                dest: 'js/build/app.js',
-            }
-        },
-        /**
-         * Uglify
-         * Minifies JS files for production
-         * 
-         */
-        uglify: {
-            build: {
-                src: 'js/build/app.js',
-                dest: 'js/build/app.min.js',
-                options: {
-                    banner: '/* HTML5 JS Boilerplate Five Design Build Date: ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
-                }
-            }
-        },
-        /**
-         * Compass settings, watches with grunt command
-         *
-         * 
-         */
-        compass: { 
-            dist: {
-                options: { 
-                    sassDir: 'sass',
-                    cssDir: 'css',
-                    outputStyle: 'compressed',
-                    imagesPath: "img",
-                    generatedImagesDir: 'img',
-                    fontsPath: "css/fonts",
-                    require: 'breakpoint',
-                    sourcemap: true
-                }
-            }
-        },
-        /**
-         * Image Optimization
-         *
-         * 
-         */
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'img/source',
-                    src: ['**/*.{png,jpg,gif,svg}'],
-                    dest: 'img/',
-                    optimizationLevel: 5
-                }],
-                png: {
-                    options: {
-                        optimizationLevel: 7
-                    }
-                }
-            }
-        },
-        /**
-         * Grunt Watch Command
-         * @ grun on cli
-         * 
-         */
-        watch: {
-            scripts: {
-                files: ['js/*.js'],
-                tasks: ['concat', 'uglify']
-            },
-            images: {
-                files: ['img/source/**/*.{png,jpg,gif}'],
-                tasks: ['imagemin']
-            },
-            styles: {
-                files: ['sass/**/*.scss'],
-                tasks: ['compass']
-            }
-        }
-    });
-    
-    /**
-     * Load NPM Tasks
-     *
-     * 
-     */
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    
-    /**
-     * Register Grunt Tasks
-     *
-     * 
-     */
-    grunt.registerTask('default', ['compass', 'concat', 'uglify', 'imagemin', 'watch']);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		/**
+		 * Concat JS Files
+		 * Contat all files into one
+		 *
+		 */
+		concat: {
+			dist: {
+				src: ['source/js/vendor-build/*.js', 'source/js/*.js', '!source/js/vendor-non-build/*.js'],
+				dest: 'source/js/build/app.js',
+			}
+		},
+		/**
+		 * Uglify
+		 * Minifies JS files for production
+		 *
+		 */
+		uglify: {
+			build: {
+				src: 'source/js/build/app.js',
+				dest: 'source/js/build/app.min.js',
+				options: {
+					banner: '/* HTML5 JS Boilerplate Five Design Build Date: ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+				}
+			}
+		},
+		/**
+		 * Compass settings, watches with grunt command
+		 *
+		 *
+		 */
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css',
+					outputStyle: 'compressed',
+					imagesDir: "source/img",
+					imagesPath: "source/img",
+					generatedImagesDir: 'source/img',
+					fontsDir: "source/css/fonts",
+					fontsPath: "source/css/fonts",
+					require: 'breakpoint',
+					sourcemap: true
+				}
+			}
+		},
+		/**
+		 * Image Optimization
+		 *
+		 *
+		 */
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'source/img/source',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'source/img/',
+					optimizationLevel: 5
+				}],
+				png: {
+					options: {
+						optimizationLevel: 7
+					}
+				}
+			}
+		},
+		/**
+		 * Grunt Copy to Build
+		 * @ grun on cli
+		 *
+		 */
+		copy: {
+			main: {
+				files: [
+				// includes files within path and its sub-directories 
+				{
+					expand: true,
+					cwd: 'source',
+					src: ['*', 'js/build/app.min.js', 'js/vendor-non-build/**', 'img/*', 'css/**', '!js/vendor-build', '!.sass-cache', '!img/source'],
+					filter: 'isFile',
+					dest: 'build/'
+				}],
+			},
+		},
+		/**
+		 * Grunt Watch Command
+		 * @ grun on cli
+		 *
+		 */
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'djrfive.com',
+					port: 21,
+					authKey: 'key1'
+				},
+				src: 'build/',
+				dest: '/public_html/build-test',
+			}
+		},
+		/**
+		 * Grunt Watch Command
+		 * @ grun on cli
+		 *
+		 */
+		watch: {
+			scripts: {
+				files: ['source/js/*.js'],
+				tasks: ['concat', 'uglify']
+			},
+			images: {
+				files: ['source/img/source/**/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
+			},
+			styles: {
+				files: ['source/sass/**/*.scss'],
+				tasks: ['compass']
+			},
+			copy: {
+				files: ['source/**'],
+				tasks: ['copy']
+			}
+		}
+	});
+	/**
+	 * Load NPM Tasks
+	 *
+	 *
+	 */
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+	/**
+	 * Register Grunt Tasks
+	 *
+	 *
+	 */
+	grunt.registerTask('default', ['compass', 'concat', 'uglify', 'imagemin', 'copy', 'ftp-deploy', 'watch']);
 };
---
+++ source/readme.md
@@ -0,0 +1,24 @@
+--------------------------------------------------
+**HTML5 + Compass + Foundation 5 + Grunt Boilerplate**
+--------------------------------------------------
+Authored by David Roach @droach587 on GitHub
+----------
+
+This is a simple boilerplate using foundation 5, modular scss, grunt and compass. 
+
+**Installation**
+
+ 1. Pull the latest master branch
+ 2. Copy and paste the entire contents of the build into your working directory
+ 3. Open your terminal and cd into your working directory or drag and drop your working folder over your terminal icon for the same effect
+ 4. Install bundler gemfile dependencies (`bundler install`)
+ 5. Once complete, install node dependencies `(npm install`)
+ 6. From there, simply execute `grunt` in your working directory, and all css, javascript and images will build and output.
+
+**Notes:**
+
+Images are generated on the fly, dump all assets into the source directory for output into img
+
+If you don't want to auto optimize images, simply add them to the /img directory
+
+compass and all JS are minified on build  
